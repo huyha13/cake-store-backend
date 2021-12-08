fd23cca373cc6d66710eedb30dde28b8a3d1b9cf
huyha13 <huyhung123ycvl@gmail.com>
2021-12-08 23:20:29 +0700

    update-promotion2

--- routes/promotions.js
+++ routes/promotions.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const router=express.Router()
 const promitionsController =require('../controllers/promitions.controller')
-router.get("/:",promitionsController.updatePromotions)
+router.put("/:id",promitionsController.updatePromotions)
 router.post("/create",promitionsController.createPromotions)
 router.delete("/:id",promitionsController.deletePromotions)
 router.get("/:id",promitionsController.getPromotionsById)
